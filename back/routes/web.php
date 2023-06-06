bcf9ca42b781a691f24adad1ce212115b74b602d
John A. Martinez <52513621+AlexanderM1927@users.noreply.github.com>
2023-06-06 13:05:12 -0500

    routes fix

--- back/routes/web.php
+++ back/routes/web.php
@@ -11,45 +11,47 @@
 |
 */
 
-$router->post('login', 'AuthController@login');
-$router->post('register', 'AuthController@register');
-$router->get('sendnotify', 'GeneralController@sendNotify');
-$router->post('user/push-token', 'AuthController@pushToken');
+$router->group(['prefix' => 'v1'], function () use ($router) {
+    $router->post('login', 'AuthController@login');
+    $router->post('register', 'AuthController@register');
+    $router->get('sendnotify', 'GeneralController@sendNotify');
+    $router->post('user/push-token', 'AuthController@pushToken');
 
-// Reports routes
-$router->get('export/{usuario_id}', 'ReportsController@ProyectStatus');
+    // Reports routes
+    $router->get('export/{usuario_id}', 'ReportsController@ProyectStatus');
 
-$router->group(['middleware' => 'auth'], function () use ($router) {
-    // Users routes
-    $router->get('user', 'UserController@getUser');
-    $router->post('user/drop-token', 'UserController@dropToken');
-    $router->post('logout', 'UserController@logout');
-    $router->get('permiss/{type}/{id}', 'UserController@hasPermissFront');
+    $router->group(['middleware' => 'auth'], function () use ($router) {
+        // Users routes
+        $router->get('user', 'UserController@getUser');
+        $router->post('user/drop-token', 'UserController@dropToken');
+        $router->post('logout', 'UserController@logout');
+        $router->get('permiss/{type}/{id}', 'UserController@hasPermissFront');
 
-    // Clients routes
-    $router->post('client', 'ClientsController@save');
-    $router->post('modify_client', 'ClientsController@update');
-    $router->post('delete_client/{id}', 'ClientsController@deleteClientById');
-    $router->get('clients/{usuario_id}', 'ClientsController@get');
-    $router->get('client/{id}', 'ClientsController@getById');
+        // Clients routes
+        $router->post('client', 'ClientsController@save');
+        $router->post('modify_client', 'ClientsController@update');
+        $router->post('delete_client/{id}', 'ClientsController@deleteClientById');
+        $router->get('clients/{usuario_id}', 'ClientsController@get');
+        $router->get('client/{id}', 'ClientsController@getById');
 
-    // Projects routes
-    $router->post('proyect', 'ProyectsController@save');
-    $router->post('modify_proyect', 'ProyectsController@update');
-    $router->post('delete_proyect/{id}', 'ProyectsController@deleteProyectById');
-    $router->get('proyects/{cliente_id}', 'ProyectsController@get');
-    $router->get('proyect/{id}', 'ProyectsController@getById');
-    $router->get('days_proyect/{id}', 'ProyectsController@getDaysProyect');
-    $router->get('last_task_proyect/{id}', 'ProyectsController@getLastTask');
-    $router->get('next_task_proyect/{id}', 'ProyectsController@getNextTask');
+        // Projects routes
+        $router->post('proyect', 'ProyectsController@save');
+        $router->post('modify_proyect', 'ProyectsController@update');
+        $router->post('delete_proyect/{id}', 'ProyectsController@deleteProyectById');
+        $router->get('proyects/{cliente_id}', 'ProyectsController@get');
+        $router->get('proyect/{id}', 'ProyectsController@getById');
+        $router->get('days_proyect/{id}', 'ProyectsController@getDaysProyect');
+        $router->get('last_task_proyect/{id}', 'ProyectsController@getLastTask');
+        $router->get('next_task_proyect/{id}', 'ProyectsController@getNextTask');
 
-    // Phases routes
-    $router->post('phases', 'PhasesController@generate');
-    $router->get('phases/{proyecto_id}', 'PhasesController@get');
+        // Phases routes
+        $router->post('phases', 'PhasesController@generate');
+        $router->get('phases/{proyecto_id}', 'PhasesController@get');
 
-    // Tasks routes
-    $router->post('task', 'TasksController@save');
-    $router->get('tasks/{fase_id}', 'TasksController@get');
-    $router->get('tasks_user/{usuario_id}', 'TasksController@getByUser');
-    $router->post('modify_task', 'TasksController@update');
+        // Tasks routes
+        $router->post('task', 'TasksController@save');
+        $router->get('tasks/{fase_id}', 'TasksController@get');
+        $router->get('tasks_user/{usuario_id}', 'TasksController@getByUser');
+        $router->post('modify_task', 'TasksController@update');
+    });
 });
